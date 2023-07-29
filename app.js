a35fe9d50d0f4985d3058a9e62f571bde076628f
sachindsilva16 <letscode16@gmail.com>
2023-07-29 10:47:57 +0530

    Add mongodb connect IP

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.static("public"));
 
 // MONGOOSE CONNECTION LINK :: AND CONNECTING/CREATING DATABASE 'toDoListItems'..
 
+// mongoose.connect("mongodb+srv://sachindsilva16:Rockerzzdown1@cluster0.0ruhws2.mongodb.net/toDoListItems", { useNewUrlParser: true });
 mongoose.connect("mongodb+srv://sachindsilva16:Rockerzzdown1@cluster0.0ruhws2.mongodb.net/toDoListItems", { useNewUrlParser: true });
 
 // DEFINE A SCHEMA FOR THE TODO ITEMS
